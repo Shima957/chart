c25b14a1aed19e62ae2d29222daaadf0f45ff7e4
Shima-Oh <shimasound04@gmail.com>
2022-04-23 19:17:10 +0900

    北海道の総人口推移をグラフ化

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
 import './App.css';
 import './reset.css';
-import { LineChart, Line, Legend, XAxis, YAxis } from 'recharts';
-import { PrefesList } from './components/PrefesList';
-import { Suspense } from 'react';
+import { Legend, Line, LineChart, XAxis, YAxis } from 'recharts';
+import axios from 'axios';
+import useSWR from 'swr';
+
+type DataType = {
+  year: number;
+  value: number;
+};
 
 function App() {
+  const url =
+    'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=11';
+
+  const fetcher = async (url: string) => {
+    const res = await axios.get(url);
+    const data = res.data.result.data[0].data;
+
+    return data;
+  };
+
+  const { data } = useSWR<DataType[]>(url, fetcher, { suspense: true });
+
   return (
     <div className='App'>
-      <Suspense fallback={<div>loading...</div>}>
-        <PrefesList />
-      </Suspense>
-      <LineChart width={730} height={250}>
-        <Legend />
-        <Line type='monotone' dataKey='uv' stroke='#8884b8' />
-        <Line type='monotone' dataKey='pv' stroke='#82ca9b' />
-        <XAxis dataKey='name' />
+      <LineChart width={730} height={250} data={data}>
+        <XAxis dataKey='year' />
         <YAxis />
+        <Legend />
+        <Line type='monotone' dataKey='value' stroke='#8884b8' />
       </LineChart>
     </div>
   );
